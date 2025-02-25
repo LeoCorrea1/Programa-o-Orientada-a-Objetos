e9b7b30ac3d825523b85604e1006b7c14411a216
Leonardo Correa <thefuzileirobr@gmail.com>
2025-02-25 09:45:43 -0300

    Add files via upload

---
+++ Exercicios lista 2/EX1/EX1.java
@@ -0,0 +1,11 @@
+package EX1;
+
+public class EX1 {
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i <= 10; i++) { 
+                System.out.println(i); 
+            
+        }
+    }
+}
