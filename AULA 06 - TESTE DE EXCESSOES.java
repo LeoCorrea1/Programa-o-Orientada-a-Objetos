8d9647bade66a3c6bb0d8d2765b709bb816ab18b
Leonardo Correa <leonardo.vasconcelos@ufn.edu.br>
2025-03-25 09:07:58 -0300

    Create AULA 06 - TESTE DE EXCESSOES.java

---
+++ AULA 06 - TESTE DE EXCESSOES.java
@@ -0,0 +1,31 @@
+package Lista3.Aula25_03;
+
+public class Principal {
+    public static void main(String[] args) {
+        int[] numeros = {1,2,3};
+        try {
+        System.out.println(numeros[99]);
+        }
+        catch ( Exception e) {
+            System.out.println("excessao : " + e.getMessage());
+            System.out.println("Excessao : " + e.toString());
+        }
+        System.out.println("programa segue em em execuçao");
+        System.out.println(numeros[3]);
+        System.out.println("Agora nao");
+
+//        //exemplo 2
+//        int a = 10;
+//        int b = 0;
+//        System.out.println(a/b);
+//
+//
+//
+//        //exemplo 3
+//        String numero = "abc";
+//        int valor = Integer.parseInt(numero);
+        
+        }
+
+    }
+
