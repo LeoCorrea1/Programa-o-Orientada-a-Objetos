a396495475d189a25bac69c87aa58433b3fb8477
Leonardo Correa <leonardo.vasconcelos@ufn.edu.br>
2025-04-01 09:17:32 -0300

    Update pessoa.java

--- AULA_01_04/pessoa.java
+++ AULA_01_04/pessoa.java
@@ -5,7 +5,7 @@ public class pessoa {
     private String nome;
     private int idade;
     public String cpf;
-    
+
     public pessoa(String nome, int idade , String cpf) {
         this.nome = nome;
         this.idade = idade;
@@ -16,6 +16,23 @@ public class pessoa {
         System.out.println("Idade: " + idade);
         System.out.println("CPF: " + cpf);
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String CPF() {
+        return cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
 
     public int getIdade() {
 
