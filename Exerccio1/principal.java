55dc9666d98b3aeee53ee51c5f85b880ac78cde6
Leonardo Correa <leonardo.vasconcelos@ufn.edu.br>
2025-04-01 09:12:54 -0300

    Add files via upload

---
+++ Exerccio1/principal.java
@@ -0,0 +1,22 @@
+package Exerccio1;
+
+public class principal {
+    public static void main(String[] args) {
+
+        pessoa p = new pessoa("leonardo" , 19 , "2132133322");
+        int idade , ano ;
+
+        idade = p.getIdade();
+        ano = 2025 - idade;
+
+        p.ApresentaDados();
+        System.out.println("ano de nascimento: " + ano);
+
+        p.setIdade(-20);
+        idade = p.getIdade();
+
+        p.setIdade(-220);
+
+    }
+
+}
