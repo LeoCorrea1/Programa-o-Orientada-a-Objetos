3d16e370294b931a199aa7198e8725a59f9cc0ee
Leonardo Correa <leonardo.vasconcelos@ufn.edu.br>
2025-04-01 09:14:24 -0300

    Add files via upload

---
+++ AULA_01_04/pessoa.java
@@ -0,0 +1,41 @@
+package Exerccio1;
+
+public class pessoa {
+
+    private String nome;
+    private int idade;
+    public String cpf;
+    
+    public pessoa(String nome, int idade , String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+    public void ApresentaDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("CPF: " + cpf);
+    }
+
+    public int getIdade() {
+
+        return idade;
+    }
+
+    public String getCpf() {
+
+        return cpf;
+    }
+
+    public void setIdade(int idade) {
+
+        if(idade < 0){
+            System.out.println("Idade negativa");
+        }
+        else {
+            this.idade = idade;
+            System.out.println("Idade: " + idade);
+        }
+    }
+
+}
