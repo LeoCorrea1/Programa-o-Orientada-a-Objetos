86aaced5401da15fb0a191d20b4d0cdadd117995
Leonardo Vasconcelos Correa <leonardo.vasconcelos@ufn.edu.br>
2025-05-26 18:13:02 -0300

    Add files via upload

---
+++ EstudoProva27.05/src/ContemTudo/OperacoesFinanceiras.java
@@ -0,0 +1,8 @@
+package ContemTudo;
+
+// ======= ExerciciosAbstract / Interface =======
+public interface OperacoesFinanceiras {
+    void depositar(double valor);
+    void sacar(double valor);
+    void exibirSaldo();
+}
